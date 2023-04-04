4652ae73618c68570f6c16be2b6d9af86bfb536c
iron-frog <124222884+iron-frog@users.noreply.github.com>
2023-04-04 20:55:24 +0100

    create function to support decimal
    Fixed answer to 2 decimal points. Next step is styling the UI.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let num2 = '';
 let operator = '';
 let equation = '';
 let result = '';
+let tempStr1 = '';
+let tempStr2 = '';
+let resultRound =0;
 
 const display = document.querySelector(".display");
 const num = document.querySelectorAll(".number");
@@ -27,6 +30,9 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    console.log("TypE: " +typeof(num2));
     if (operator == '') {
         return '0';
     }
@@ -40,7 +46,8 @@ function operate(operator, num1, num2) {
         return multiply(num1, num2)
     }
     if (operator == "/") {
-        if (num2 == 0) {
+        if (num2 === 0) {
+            console.log("helloooo");
             return "Error"
         }
         return divide(num1, num2)
@@ -50,34 +57,48 @@ function operate(operator, num1, num2) {
 
 function calc() {
     num.forEach(btn => (btn.addEventListener('click', (e) => {
+        tempStr1 = checkDecimal(num1.concat('', e.target.textContent));
+        tempStr2 = checkDecimal(num2.concat('', e.target.textContent));
+
         if (operator == '=') {
-            num1 = ''.concat('', e.target.textContent);
-            operator = '';
-            updateDisplay(num1);
-            console.log(num1);
-
-        } else
-            if (operator === '') {
-
-                num1 = num1.concat('', e.target.textContent);
+            if (tempStr1 != 2) {
+                num1 = ''.concat('', e.target.textContent);
+                operator = '';
                 updateDisplay(num1);
-
+                console.log(num1);
             }
             else {
+                return;
+            }
+        } else if (operator === '') {
+            if (tempStr1 != 2) {
+                num1 = num1.concat('', e.target.textContent);
+                updateDisplay(num1);
+            } else {
+                return;
+            }
+        }
+        else {
+            if (tempStr2 != 2) {
                 num2 = num2.concat('', e.target.textContent);
                 updateDisplay(num2);
-
             }
+            else {
+                return;
+            }
+        }
         console.log("current op: " + operator);
         console.log("num1: " + num1);
         console.log("num2: " + num2);
         console.log("result: " + result);
 
+
+
     })));
     op.forEach(btn => (btn.addEventListener('click', (e) => {
         if (num2 != '') {
             //console.log("hello");
-            result = operate(operator, parseInt(num1), parseInt(num2));
+            result = operate(operator, num1, num2);
             operator = e.target.textContent;
             num2 = '';
             num1 = result;
@@ -91,32 +112,51 @@ function calc() {
         console.log("num2: " + num2);
         console.log("result: " + result);
 
-
     })));
 
     equals.addEventListener("click", () => {
-        console.log(operator, num1, num2);
+        //console.log(operator, num1, num2);
 
-        result = operate(operator, parseInt(num1), parseInt(num2));
-        updateDisplay(result);
+        if(num2 !=''){
+            result = operate(operator, num1, num2);
+        }
+        else{
+            result = 0;
+        }
+        if(typeof(result)=="number"){
+            console.log(result.toFixed(2));
+            resultRound = result.toFixed(2);
+        }
+        
+        updateDisplay(resultRound);
+        console.log(result);
+
         num1 = result.toString();
         num2 = '';
         operator = '=';
-        //result = ''
+    
         console.log("current op: " + operator);
         console.log("num1: " + num1);
         console.log("num2: " + num2);
         console.log("result: " + result);
     });
 
-    equation = num1 + operator + num2;
-    console.log(equation);
-    checkError(result);
+}
 
+function checkDecimal(str) {
+    let sum = 0;
+    for (let i in str) {
+        if (str[i] == '.') {
+            sum += 1;
+        }
+    }
+    return sum;
 }
 
 function checkError(str) {
     if (str == 'Error') {
+        console.log("hhh")
+
         clear();
     }
 }
